e42efa453025714554102aca680bb4e0bbcf4f59
Linus Shyu <83543818+Linus-Shyu@users.noreply.github.com>
2026-01-31 21:13:42 +0800

    Update starfetch.rb

--- Formula/starfetch.rb
+++ Formula/starfetch.rb
@@ -1,22 +1,22 @@
 class Starfetch < Formula
   desc "A high-performance system information tool written in Rust"
   homepage "https://github.com/Linus-Shyu/StarFetch_Core"
-  # 这里的 URL 和 SHA256 会被 GitHub Action 自动更新
+  # 注意：这里的 URL 和 SHA256 会被机器人自动更新，现在可以先放个占位符
   url "https://github.com/Linus-Shyu/StarFetch_Core/archive/refs/tags/v0.2.0.tar.gz"
-  sha256 "2fdf404575dad321a086d5706b2e5929eb4d388393ae72e60637437c0a3358bb"
+  sha256 "0000000000000000000000000000000000000000000000000000000000000000"
   license "MIT"
 
   depends_on "rust" => :build
 
   def install
-    # 既然 StarFetch 是 Rust 项目，使用这个标准命令即可
-    # 它会自动执行 cargo build --release 并把二进制文件移到 Homebrew 的 bin 目录
-    system "cargo", "install", *std_cargo_args
+    # 关键修正：进入代码所在的子目录
+    Dir.chdir "StarFetch" do
+      system "cargo", "install", *std_cargo_args
+    end
   end
 
   test do
-    # 简单的安装后测试
+    # 简单的版本测试
     assert_match "starfetch", shell_output("#{bin}/starfetch --version")
   end
 end
-
