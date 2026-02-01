b105c2bf8b4102ee354b99ba51c0358ef0702743
Linus Shyu <83543818+Linus-Shyu@users.noreply.github.com>
2026-02-01 15:20:56 +0800

    starfetch 0.2.4
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/starfetch.rb
+++ Formula/starfetch.rb
@@ -2,8 +2,8 @@ class Starfetch < Formula
   desc "A high-performance system information tool written in Rust"
   homepage "https://github.com/Linus-Shyu/StarFetch_Core"
   # 注意：这里的 URL 和 SHA256 会被机器人自动更新，现在可以先放个占位符
-  url "https://github.com/Linus-Shyu/StarFetch_Core/releases/download/v0.2.3/starfetch-macos-x86_64.tar.gz"
-  sha256 "08567e179c475f12361e9500ad3afa6bde27690d5bdb02b23a2aea5fb4545d0d"
+  url "https://github.com/Linus-Shyu/StarFetch_Core/releases/download/v0.2.4/starfetch-macos-x86_64.tar.gz"
+  sha256 "79b3fb9beed58fa01e6fa7316fe4286a6141ba342a6305b5556f4a4931a6c67a"
   license "MIT"
 
   depends_on "rust" => :build
